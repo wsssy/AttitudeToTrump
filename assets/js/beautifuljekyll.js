30ea42f83b580c537f1a2fcc471799c4a9fcb12b
Huan Yang <44476920+huan-YH@users.noreply.github.com>
2021-12-16 08:25:42 +0800

    Update beautifuljekyll.js
    add sidenote support

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -140,3 +140,24 @@ var BeautifulJekyllJS = {
 // 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', BeautifulJekyllJS.init);
+
+window.addEventListener("load", (event) => {
+  let observer;
+
+  let options = {
+    root: null,
+    rootMargin: '-25% 0px -50% 0px'
+  };
+
+  observer = new IntersectionObserver(handleIntersect, options);
+  for (const sectionEl of document.querySelectorAll(".withSidenote"))
+    observer.observe(sectionEl)
+})
+
+function handleIntersect(entries) {
+  for (const entry of entries) {
+    const noteEl = entry.target.querySelector(".sidenote");
+    if (entry.isIntersecting) noteEl.classList.add("display")
+    else noteEl.classList.remove("display")
+  }
+};
